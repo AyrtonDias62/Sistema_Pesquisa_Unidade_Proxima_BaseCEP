856262e64beefd76eee2261587b291785897078e
AyrtonDias62 <ayrton.bdias@gmail.com>
2026-03-19 20:13:40 -0300

    Update app_Pesquisa_CEP.py

--- app_Pesquisa_CEP.py
+++ app_Pesquisa_CEP.py
@@ -104,7 +104,7 @@ with c_logo:
     try: st.image("furgao_tecnolab.png", width=220)
     except: st.warning("🚚 Imagem não encontrada")
 with c_tit:
-    st.markdown('<div class="header-container"><h1 class="titulo-v86">Painel Localizador CEP Cliente x Unidade Tecnolab mais próxima</h1></div>', unsafe_allow_html=True)
+    st.markdown('<div class="header-container"><h1 class="titulo-v86">Localizador CEP Cliente x Un. Tecnolab mais próxima...</h1></div>', unsafe_allow_html=True)
 
 if 'historico' not in st.session_state: st.session_state['historico'] = []
 
